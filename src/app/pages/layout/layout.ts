1d7214b032d5c3adc0e2eff5f4665560aa67143d
Github Projects <barik011@gmail.com>
2025-12-30 18:33:05 +0530

    Batch Recordning Login Work

---
+++ src/app/pages/layout/layout.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterOutlet],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css',
+})
+export class Layout {
+
+  loggedInData:any;
+
+  router = inject(Router);
+
+  constructor(){
+    const localData = localStorage.getItem('batch32');
+    if(localData != null){
+      this.loggedInData = JSON.parse(localData)
+    }
+  }
+
+  onLogOff(){
+    localStorage.removeItem('batch32');
+    this.router.navigate(['login']);
+  }
+}
---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+  http = inject(HttpClient);
+  router = inject(Router)
+  loginObj:any = {
+  email: "",
+  password: ""
+}
+
+onLogin(){
+  debugger;
+  this.http.post('https://feestracking.freeprojectapi.com/api/BatchUser/login',this.loginObj).subscribe({
+    next:(result:any)=>{
+      debugger;
+      localStorage.setItem('batch32',JSON.stringify(result.data));
+      this.router.navigateByUrl('dashboard');
+    },
+    error:(err:any)=>{
+      debugger;
+      alert(err.error.message)
+    }
+  })
+
+}
+
+
+}
