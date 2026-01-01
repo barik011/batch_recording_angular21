1324a0aeb54f7efb8283a14e0400eb8fac50ea34
Github Projects <barik011@gmail.com>
2026-01-01 23:50:17 +0530

    srv created

---
+++ src/app/services/login-services.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginServices {
+  loginApiUrl='https://feestracking.freeprojectapi.com/api/';
+  http = inject(HttpClient)
+    onLoginServ(obj:any){
+      this.http.post(`${this.loginApiUrl}'BatchUser/login'`, obj);
+    }
+}
