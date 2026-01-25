fc392bf32a48f2718b6a129d209381fc64ba4a1b
Github Projects <barik011@gmail.com>
2026-01-25 20:23:05 +0530

    Authorization

--- src/app/core/interceptor/token-interceptor.ts
+++ src/app/core/interceptor/token-interceptor.ts
@@ -1,5 +1,12 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  debugger;
+  const token = localStorage.getItem('batchToken');
+  const newRequest = req.clone({
+    setHeaders:{
+      Authorization:`Bearer ${token}`
+    }
+  })
+  return next(newRequest);
 };
